bd46ddf3d42973e40ebcae765f8ba4049798ede7
Ty Downs <tdowns@ghrr.com>
2019-05-31 18:48:36 -0400

    First pass.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-//asdf
+$(window).scroll(onScroll);
+onScroll();
+
+function onScroll(){
+  var opacity = ($(window).scrollTop() / $(window).height());
+  $('#mainNav').css('background-color', 'rgb(37,41,52,' + opacity + ')');
+}
+
+$('body').scrollspy({
+  target: '#mainNav',
+  offset: 75
+});
